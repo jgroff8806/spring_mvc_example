7ff803a7400a2887b698b359290dace088fd91a0
Jason Groff <jason@snapit.solutions>
2024-06-19 12:23:51 -0500

    Spring MVC Example

---
+++ src/main/java/com/example/spring_mvc_example/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.spring_mvc_example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
